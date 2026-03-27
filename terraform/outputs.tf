5ef89ddc04e2055116d9d7a9aba6fc3c82b07e49
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2026-03-27 14:07:33 +0900

    feat(terraform): add outputs for missing scheduler jobs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -39,3 +39,18 @@ output "group_collection_scheduler_job" {
   value       = google_cloud_scheduler_job.group_collection_daily.name
   description = "Cloud Scheduler job name for daily Google group collection."
 }
+
+output "reconciliation_scheduler_job" {
+  value       = google_cloud_scheduler_job.reconciliation_daily.name
+  description = "Cloud Scheduler job name for daily reconciliation."
+}
+
+output "revoke_expired_permissions_scheduler_job" {
+  value       = google_cloud_scheduler_job.revoke_expired_permissions_daily.name
+  description = "Cloud Scheduler job name for daily revocation of expired permissions."
+}
+
+output "iam_bindings_history_update_scheduler_job" {
+  value       = google_cloud_scheduler_job.iam_bindings_history_update_daily.name
+  description = "Cloud Scheduler job name for daily update of IAM bindings history."
+}
