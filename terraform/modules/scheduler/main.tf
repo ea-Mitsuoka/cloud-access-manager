f9710a0ead87cfcd686c91b66271c05ded519510
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2026-04-07 02:26:00 +0900

    chore: minor code fixes

--- terraform/modules/scheduler/main.tf
+++ terraform/modules/scheduler/main.tf
@@ -161,9 +161,9 @@ resource "google_cloud_scheduler_job" "iam_policy_collection_daily" {
 }
 
 resource "google_cloud_scheduler_job" "iam_role_discovery_daily" {
-  name             = "iam-role-discovery-daily"
-  project          = var.tool_project_id
-  region           = var.region
+  name    = "iam-role-discovery-daily"
+  project = var.tool_project_id
+  region  = var.region
   # 既存インフラの変数を汚さないよう、不整合検知(04:00)の後の 04:30 AM をハードコード指定
   schedule         = "30 4 * * *"
   time_zone        = var.scheduler_time_zone
